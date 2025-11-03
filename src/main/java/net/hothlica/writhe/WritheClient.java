3426cc3118f1143efaad7518d775fe3a210c94c2
Jayden Kim <jaydenki@umich.edu>
2025-11-02 23:13:25 -0500

    Initial commit

---
+++ src/main/java/net/hothlica/writhe/WritheClient.java
@@ -0,0 +1,10 @@
+package net.hothlica.writhe;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class WritheClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
